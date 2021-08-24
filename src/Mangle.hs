93b16bf0375a2d39693a20a04ea8dd5b32f205ae
Jason Nguyen <hpj2ltxry43b@gmail.com>
2021-08-24 15:08:23 -0400

    add todos

--- src/Mangle.hs
+++ src/Mangle.hs
@@ -24,10 +24,10 @@ to_mn :: Tag -> MangledName
 to_mn = MangledName . str_tag
 
 mangle_dsidx :: IR.DSIdx d -> MangledName
-mangle_dsidx dsidx = to_mn $ Tag tn'dsidx [StrTag tn'idx (show $ IR.upcast_dsidx dsidx)]
+mangle_dsidx dsidx = to_mn $ Tag tn'dsidx [StrTag tn'idx (show $ IR.upcast_dsidx dsidx)] -- TODO: do this better
 
 mangle_vidx :: IR.VIdx v -> MangledName
-mangle_vidx vidx = to_mn $ Tag tn'vidx [StrTag tn'idx (show $ IR.upcast_vidx vidx)]
+mangle_vidx vidx = to_mn $ Tag tn'vidx [StrTag tn'idx (show $ IR.upcast_vidx vidx)] -- TODO: do this better also
 
 mangle_fun :: InternerIdx IR.Function -> MangledName
 mangle_fun fidx = to_mn $ Tag tn'funidx [StrTag tn'idx (show fidx)]
